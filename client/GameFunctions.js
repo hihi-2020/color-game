3d7425772cb09786149661a236efac523e045300
student <student@eda-bc-04.localdomain>
2020-11-06 13:18:32 +1300

    Word changes color

--- client/GameFunctions.js
+++ client/GameFunctions.js
@@ -8,42 +8,56 @@ let styleColor = ""
 let word = ""
 
 // randomly selects a color pair from the array of all colors
-  function getPair(){
+  export function getPair(){
 
     colorPair = allColorsArray[Math.floor(Math.random() * allColorsArray.length)]
-    console.log("colorPair : " + colorPair) 
+    console.log("colorPair : " + colorPair)
+    return colorPair
   }
 
   // accesses the color text string from the pair **THE ANSWER**
-  function getAnswer(){
+  export function getAnswer(){
     answer = colorPair[0]
     console.log("answer: " + answer)
+    return answer
   }
 
   // accesses the color hex value from the pair **THE STYLING TO APPLY**
-  function getStyle(){
+  export function getStyle(){
     styleColor = colorPair[1]
     console.log("styleColor: " + styleColor)
+    return styleColor
   }
 
 
   // gets a new random pair and takes the string of text to use as the WORD that is STYLED by the ANSWER color
-  function getWord(){
+export function getWord(){
   word = allColorsArray[Math.floor(Math.random() * allColorsArray.length)][0]
-  console.log("word: " + word)
-
+  // console.log("word: " + word)
+    return word
 }
 
-getPair()
-getAnswer()
-getStyle()
-getWord()
+  // function evaluateResult(){
+  //   if (document.getElementsByClassName(answerInput).value == answer {
+      
+  //   } 
+  // }
 
-  module.export = {
-    getPair,
-    getAnswer,
-    getStyle,
-    getWord
-    }
+
+
+
+
+
+// getPair()
+// getAnswer()
+// getStyle()
+// getWord()
+
+  // module.exports = {
+  //   getPair,
+  //   getAnswer,
+  //   getStyle,
+  //   getWord,
+  //   }
 
   // export default GameFunctions
--- client/components/MainArea.jsx
+++ client/components/MainArea.jsx
@@ -1,14 +1,58 @@
 import React from "react";
 
+import {getAnswer, getWord} from '../GameFunctions'
+import {getStyle} from '../GameFunctions'
+import {getPair} from '../GameFunctions'
+
 class MainArea extends React.Component {
+    state = {
+        word: '',
+        style: {
+        color: 'blue'
+        }
+    }
+
+    componentDidMount() {
+        this.generateColour()
+        // this.getStyle()
+    }
+
+    generateColour = () => {
+        const pair = getPair()
+        const answer = getAnswer()
+        const word = getWord()
+        const style = getStyle()
+        this.setState({
+            word: word,
+            style: {
+                ...this.state.style,
+                color: style
+            }
+            
+
+        })
+        console.log(word)
+        console.log("style: " + style)
+    }
+  
   render() {
     return (
       <div>
-        <h1>yellow.</h1>
-        <input type="text" />
+        <p type="text" className="wordInput" style={this.state.style}>{this.state.word}</p>
+        <button onClick={this.generateColour}>Go Again?</button>
+
+        {/* <input type="text" className="answerInput"></input> */}
       </div>
     );
   }
 }
+
+// state = {
+//     style: {
+//         color: this.styleColor
+//     }
+
+// }
+
 
 export default MainArea;
